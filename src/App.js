db6f6ee090cee65eb5537bb8abdb2cde34044a40
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-09-17 23:49:54 +0530

    feat: Add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
 import './App.css';
-import { Register } from './components';
+import { DogListing,DogDetails } from './pages';
+// import { Register } from './components';
+import { Routes, Route } from "react-router-dom";
+
+
 
 function App() {
   return (
     <div className="App">
-    <Register />
+      <Routes>
+      <Route path="/" element={<DogListing />} />
+      <Route path="/dogdetails" element={<DogDetails />} />
+      </Routes>
+    {/* <Register /> */}
+{/* <DogListing /> */}
     </div>
   );
 }
