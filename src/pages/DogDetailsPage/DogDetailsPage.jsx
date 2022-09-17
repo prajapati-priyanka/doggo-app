283d363da115be01c1d327ca8396a9456e3aeb38
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-09-17 23:48:12 +0530

    feat: Add DogDetails Page

---
+++ src/pages/DogDetailsPage/DogDetailsPage.jsx
@@ -0,0 +1,9 @@
+import DogDetailsStyles from "./DogDetailsPage.module.css";
+
+const DogDetails = () => {
+    return ( 
+        <h1>This is dog details page</h1>
+     );
+}
+ 
+export {DogDetails};
